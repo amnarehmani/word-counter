fc7c9f219c90e6efab5c4c05619bf4c405dc29bf
amnarehmani <jeonamnaaa@gmail.com>
2024-06-14 20:47:14 +0500

    Word Counter

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+// declaring a variable and assign it to the inquirer prompt
+let wordsAnswers = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word"
+    }]);
+const words = wordsAnswers.Sentence.trim().split(" ");
+// Print the array of words to the console
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
